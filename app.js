c504a7306e3f20d849285cb94c31b9e3ff51de26
Shinichi Tomita <shinichi.tomita@gmail.com>
2015-08-10 13:56:15 +0900

    add jwt rs256 authentication

--- app.js
+++ app.js
@@ -1,53 +1,34 @@
+var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var express = require('express');
+var session = require('express-session');
 var RED = require('node-red');
 var settings = require('./settings');
 var app = express();
-app.use('/red', express.static('public'));
 var server = http.createServer(app);
 
-if (process.env.NODE_RED_USERNAME && process.env.NODE_RED_PASSWORD) {
-  console.log(process.env.NODE_RED_USERNAME, process.env.NODE_RED_PASSWORD);
-  settings.adminAuth = {
-    type: "credentials",
-    users: function(username) {
-      console.log('users', username);
-      if (process.env.NODE_RED_USERNAME == username) {
-        return when.resolve({username:username,permissions:"*"});
-      } else {
-        return when.resolve(null);
-      }
-    },
-    authenticate: function(username, password) {
-      console.log('authenticate', username, password);
-      if (process.env.NODE_RED_USERNAME == username &&
-        process.env.NODE_RED_PASSWORD == password) {
-        return when.resolve({username:username,permissions:"*"});
-      } else {
-        return when.resolve(null);
-      }
-    }
-  }
+app.use(session({ secret: '4r13ysgyYD' }));
 
+var JWTAuth = require('./auth/jwt');
+
+if (process.env.PUBLIC_KEY_PATH) {
+  app.use(JWTAuth(process.env.PUBLIC_KEY_PATH, {
+    issuer: process.env.ISSUER,
+    audience: process.env.AUDIENCE,
+  }));
+} else if (process.env.NODE_RED_USERNAME && process.env.NODE_RED_PASSWORD) {
   settings.adminAuth = {
     type: 'credentials',
     users: [{
       username: process.env.NODE_RED_USERNAME,
       password: require('bcryptjs').hashSync(process.env.NODE_RED_PASSWORD, 8),
-      permissions: '*'
-    }]
-  };
-} else {
-  settings.adminAuth = {
-    type: 'credentials',
-    users: [{
-      username: 'admin',
-      password: '$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.',
-      permissions: '*'
+      permissions: '*',
     }]
   };
 }
+
+app.use('/red', express.static('public'));
 
 RED.init(server, settings);
 app.use(settings.httpAdminRoot, RED.httpAdmin);
