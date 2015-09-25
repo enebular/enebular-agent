359504f91c004af3c2c679c96d434d4633e361fc
Shuhei Hiya <syuhei176@gmail.com>
2015-09-25 16:07:30 +0900

    enebular agent初期設定 #8

---
+++ server/agentid.js
@@ -0,0 +1,25 @@
+var uuid = require('uuid');
+var store = require('../store');
+
+module.exports = {
+	agent_id : null,
+	init : function(cb) {
+		var that = this;
+		store.get('agentid', function(err, agent_id) {
+			if(agent_id) {
+				if(cb) cb(err, agent_id)
+			}else{
+				agent_id = uuid.v1();
+				store.set('agentid', agent_id, function(err) {
+					if(cb) cb(err, agent_id);
+				});
+			}
+			that.agent_id = agent_id;
+			console.log('agent_id', agent_id);
+		});
+
+	},
+	getAgentId : function() {
+		return this.agent_id;
+	}
+}
